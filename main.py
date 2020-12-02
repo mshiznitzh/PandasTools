81164ae0d93966660940baaebd3aedf8bbbeed9b
Mike Howard <mshiznitzh@gmail.com>
2020-12-02 07:31:49 -0600

    Create version Control

---
+++ main.py
@@ -0,0 +1,48 @@
+''''''
+
+import logging
+import datetime as DT
+import tkinter as tk
+
+def Excel_to_Pandas(filename,check_update=False):
+    logger.info('importing file ' + filename)
+    df=[]
+    if check_update == True:
+        timestamp = DT.datetime.fromtimestamp(Path(filename).stat().st_mtime)
+        if DT.datetime.today().date() != timestamp.date():
+            root = tk.Tk()
+            root.withdraw()
+            filename = filedialog.askopenfilename(title =' '.join(['Select file for', filename]))
+
+    try:
+        df = pd.read_excel(filename, sheet_name=None)
+        df = pd.concat(df, axis=0, ignore_index=True)
+    except:
+        logger.error("Error importing file " + filename, exc_info=True)
+
+    df=Cleanup_Dataframe(df)
+    logger.debug(df.info(verbose=True))
+    return df
+
+def Cleanup_Dataframe(df):
+    logger.info('Starting Cleanup_Dataframe')
+    logger.debug(df.info(verbose=True))
+    # Remove whitespace on both ends of column headers
+    df.columns = df.columns.str.strip()
+
+    # Replace whitespace in column header with _
+    df.columns = df.columns.str.replace(' ', '_')
+
+    return df
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
